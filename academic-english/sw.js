c928651e83bf0dcb1cd32250cfc6540df0c22fcc
Łukasz Żyła <lukaszyla@gmail.com>
2026-09-20 09:26:07 +0200

    Refresh PWA cache for install assets

--- academic-english/sw.js
+++ academic-english/sw.js
@@ -1,5 +1,5 @@
-const CACHE='legal-english-v2';
-const ASSETS=['./','./index.html','./data1.js','./data2.js','./data3.js','./data4.js','./app.js','./manifest.webmanifest'];
-self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS))));
+const CACHE='legal-english-v3';
+const ASSETS=['./','./index.html','./data1.js','./data2.js','./data3.js','./data4.js','./app.js','./manifest.webmanifest','./icon-192.svg','./icon-512.svg'];
+self.addEventListener('install',e=>{self.skipWaiting();e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)))});
 self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
 self.addEventListener('fetch',e=>e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request))));
